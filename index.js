ff3e79f2b6196b9ca13b20a2c1e6ce8ebef2bc69
shagan15 <nihushagans@gmail.com>
2023-12-12 00:15:53 +0530

    new

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 const database = require("./database");
 
 const booky = express();
+
+
+booky.use(bodyParser.urlencoded({extended: true}));
+booky.use(bodyParser.json());
+
+
+mongoose.connect("",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFineAndModify: false,
+  useCreateIndex: true
+}
+).then(() => console.log("Connection established!"));
 
 /*
 Route               /
@@ -29,9 +45,8 @@ booky.get("/is/:isbn", (req,res) => {
   if(getSpecificBook.length === 0) {
     return res.json({error: `No book found for the ISBN of ${req.params.isbn}`});
   }
-  else{
-    return res.json({book: getSpecificBook});
-  }
+
+  return res.json({book: getSpecificBook});
 });
 
 booky.listen(3000, () => {
